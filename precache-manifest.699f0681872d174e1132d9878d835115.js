fe8683d36a7f911421d5452bb0482af29f2ebd0a
PSheon <pauljiang61020@gmail.com>
2020-07-22 23:43:36 +0800

    Updates

---
+++ precache-manifest.699f0681872d174e1132d9878d835115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94499773f5f81e4ca147cba28902ca9",
+    "url": "/hiifx_management/index.html"
+  },
+  {
+    "revision": "7ada863acf899f3a549e",
+    "url": "/hiifx_management/static/css/2.f090b26d.chunk.css"
+  },
+  {
+    "revision": "ff9883c0a7a3b18ea20d",
+    "url": "/hiifx_management/static/css/main.3b7d4e9e.chunk.css"
+  },
+  {
+    "revision": "7ada863acf899f3a549e",
+    "url": "/hiifx_management/static/js/2.bd816c3d.chunk.js"
+  },
+  {
+    "revision": "407639d8cafb6082e44e6799163e97d3",
+    "url": "/hiifx_management/static/js/2.bd816c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff9883c0a7a3b18ea20d",
+    "url": "/hiifx_management/static/js/main.121de75b.chunk.js"
+  },
+  {
+    "revision": "10ab028616317d6a2380",
+    "url": "/hiifx_management/static/js/runtime-main.57b51508.js"
+  }
+]);
