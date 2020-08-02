cd8955a285f4337515f2b11664aa75b4190e84d4
PSheon <pauljiang61020@gmail.com>
2020-08-02 23:33:29 +0800

    feat: added new testing

--- src/components/Toolbar/Toolbar.test.js
+++ src/components/Toolbar/Toolbar.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
+import Toolbar from "./Toolbar";
 
-test("renders logo", () => {
-  const { getByText } = render(<App />);
+test("renders toolbar", () => {
+  const { getByText } = render(<Toolbar />);
   const linkElement = getByText(/海匯團隊資金管理工具/i);
   expect(linkElement).toBeInTheDocument();
 });
